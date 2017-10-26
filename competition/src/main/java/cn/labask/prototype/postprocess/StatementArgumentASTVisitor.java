2ab62a8b9b5e949b76f35263b391a538277b347d
yyx <yyx@yyx-PC>
2017-10-26 17:52:36 +0800

    small change.

--- competition/src/main/java/cn/labask/prototype/postprocess/StatementArgumentASTVisitor.java
+++ competition/src/main/java/cn/labask/prototype/postprocess/StatementArgumentASTVisitor.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.ConstructorInvocation;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
@@ -15,17 +17,17 @@ import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import cn.labask.prototype.util.NumericUtil;
 
 public class StatementArgumentASTVisitor extends ASTVisitor {
-	
+
 	int arg_count = 0;
 	AST ast = null;
 	ASTRewrite rewrite = null;
 	MethodDeclaration node = null;
 	boolean first = true;
-	
+
 	StringBuilder declare_func_name = new StringBuilder("public static void ");
 	StringBuilder invoke = new StringBuilder("");
 	StringBuilder config = new StringBuilder("");
-	
+
 	public StatementArgumentASTVisitor(AST ast, ASTRewrite rewrite, MethodDeclaration node, String func_name) {
 		this.ast = ast;
 		this.rewrite = rewrite;
@@ -35,49 +37,74 @@ public class StatementArgumentASTVisitor extends ASTVisitor {
 		invoke.append(func_name);
 		config.append(func_name);
 	}
-	
+
 	@Override
-	public boolean visit(MethodInvocation node) {
-		IMethodBinding nbinding = node.resolveMethodBinding();
-		// System.err.println(nbinding);
+	public boolean visit(ConstructorInvocation node) {
+		IMethodBinding nbinding = node.resolveConstructorBinding();
 		if (nbinding != null && node.arguments() != null && node.arguments().size() > 0) {
 			ITypeBinding[] it_args = nbinding.getParameterTypes();
-			int index = 0;
 			@SuppressWarnings("unchecked")
-			List<Expression> args = (List<Expression>)node.arguments();
-			for (Expression arg : args) {
-				ITypeBinding itb = it_args[index];
-				String arg_cnt = arg.toString().trim();
-				
-				// testing.
-				// System.err.println(arg_cnt + "# NumericUtil.IsPrmitive #" + NumericUtil.IsPrmitive(arg_cnt) + "#" + arg.resolveConstantExpressionValue());
-				
-				String itb_qualified = itb.getQualifiedName();
-				if (itb_qualified.equals("boolean") || itb_qualified.equals("byte") || itb_qualified.equals("short") || itb_qualified.equals("int") || itb_qualified.equals("long") || itb_qualified.equals("float") || itb_qualified.equals("double")  || itb_qualified.equals("java.lang.Boolean") || itb_qualified.equals("java.lang.Byte") || itb_qualified.equals("java.lang.Short") || itb_qualified.equals("java.lang.Integer") || itb_qualified.equals("java.lang.Long") || itb_qualified.equals("java.lang.Float") || itb_qualified.equals("java.lang.Double")) {
-					if (NumericUtil.IsPrmitive(arg)) {
-						arg_count++;
-						SimpleName s = ast.newSimpleName("x" + arg_count);
-						rewrite.replace(arg, s, null);
-						String pre = "(";
-						if (first) {
-							first = false;
-						} else {
-							pre = ",";
-						}
-						declare_func_name.append(pre + itb_qualified + " " + s.toString());
-						invoke.append(pre + arg_cnt);
-						config.append(pre + s.toString() + ":" + itb_qualified);
-					}
-				}
-				index++;
-			}
-		} else {
-//			System.out.println("===================");
-//			System.out.println(node);
+			List<Expression> args = (List<Expression>) node.arguments();
+			HandleMethodInvocation(it_args, args);
 		}
 		return super.visit(node);
 	}
-	
+
+	@Override
+	public boolean visit(ClassInstanceCreation node) {
+		IMethodBinding nbinding = node.resolveConstructorBinding();
+		if (nbinding != null && node.arguments() != null && node.arguments().size() > 0) {
+			ITypeBinding[] it_args = nbinding.getParameterTypes();
+			@SuppressWarnings("unchecked")
+			List<Expression> args = (List<Expression>) node.arguments();
+			HandleMethodInvocation(it_args, args);
+		}
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(MethodInvocation node) {
+		IMethodBinding nbinding = node.resolveMethodBinding();
+		if (nbinding != null && node.arguments() != null && node.arguments().size() > 0) {
+			ITypeBinding[] it_args = nbinding.getParameterTypes();
+			@SuppressWarnings("unchecked")
+			List<Expression> args = (List<Expression>) node.arguments();
+			HandleMethodInvocation(it_args, args);
+		}
+		return super.visit(node);
+	}
+
+	private void HandleMethodInvocation(ITypeBinding[] it_args, List<Expression> args) {
+		int index = -1;
+		for (Expression arg : args) {
+			index++;
+			ITypeBinding itb = it_args[index];
+			String arg_cnt = arg.toString().trim();
+			String itb_qualified = itb.getQualifiedName();
+			if (itb_qualified.equals("boolean") || itb_qualified.equals("byte") || itb_qualified.equals("short")
+					|| itb_qualified.equals("int") || itb_qualified.equals("long") || itb_qualified.equals("float")
+					|| itb_qualified.equals("double") || itb_qualified.equals("java.lang.Boolean")
+					|| itb_qualified.equals("java.lang.Byte") || itb_qualified.equals("java.lang.Short")
+					|| itb_qualified.equals("java.lang.Integer") || itb_qualified.equals("java.lang.Long")
+					|| itb_qualified.equals("java.lang.Float") || itb_qualified.equals("java.lang.Double")) {
+				if (NumericUtil.IsPrmitive(arg)) {
+					arg_count++;
+					SimpleName s = ast.newSimpleName("x" + arg_count);
+					rewrite.replace(arg, s, null);
+					String pre = "(";
+					if (first) {
+						first = false;
+					} else {
+						pre = ",";
+					}
+					declare_func_name.append(pre + itb_qualified + " " + s.toString());
+					invoke.append(pre + arg_cnt);
+					config.append(pre + s.toString() + ":" + itb_qualified);
+				}
+			}
+		}
+	}
+
 	@Override
 	public void endVisit(MethodDeclaration node) {
 		if (this.node.equals(node)) {
@@ -91,7 +118,7 @@ public class StatementArgumentASTVisitor extends ASTVisitor {
 	public String GetFunctionName() {
 		return declare_func_name.toString();
 	}
-	
+
 	public String GetConfig() {
 		return config.toString();
 	}
@@ -99,9 +126,9 @@ public class StatementArgumentASTVisitor extends ASTVisitor {
 	public String GetInvoke() {
 		return invoke.toString();
 	}
-	
+
 	public boolean NeedJdartExecution() {
 		return arg_count > 0;
 	}
-	
+
 }
